a45e23d4d656ffc7c7bc0055cd8e3ee840c91632
MelisaCaro <melisaailencaroo@gmail.com>
2026-06-29 17:21:03 -0300

    mod de roles

--- db/Roles.sql
+++ db/Roles.sql
@@ -27,6 +27,34 @@ USE ParquesNacionalesDB;
 GO
 
 
+IF EXISTS (SELECT 1 FROM sys.database_principals WHERE name = 'usr_admin')
+    ALTER ROLE rol_admin DROP MEMBER usr_admin;
+IF EXISTS (SELECT 1 FROM sys.database_principals WHERE name = 'usr_ventas')
+    ALTER ROLE rol_ventas DROP MEMBER usr_ventas;
+IF EXISTS (SELECT 1 FROM sys.database_principals WHERE name = 'usr_consulta')
+    ALTER ROLE rol_consulta DROP MEMBER usr_consulta;
+IF EXISTS (SELECT 1 FROM sys.database_principals WHERE name = 'usr_importador')
+    ALTER ROLE rol_importador DROP MEMBER usr_importador;
+GO
+ 
+IF EXISTS (SELECT 1 FROM sys.database_principals WHERE name = 'usr_admin'      AND type = 'U') DROP USER usr_admin;
+IF EXISTS (SELECT 1 FROM sys.database_principals WHERE name = 'usr_ventas'     AND type = 'U') DROP USER usr_ventas;
+IF EXISTS (SELECT 1 FROM sys.database_principals WHERE name = 'usr_consulta'   AND type = 'U') DROP USER usr_consulta;
+IF EXISTS (SELECT 1 FROM sys.database_principals WHERE name = 'usr_importador' AND type = 'U') DROP USER usr_importador;
+GO
+ 
+IF EXISTS (SELECT 1 FROM sys.server_principals WHERE name = 'usr_admin')       DROP LOGIN usr_admin;
+IF EXISTS (SELECT 1 FROM sys.server_principals WHERE name = 'usr_ventas')      DROP LOGIN usr_ventas;
+IF EXISTS (SELECT 1 FROM sys.server_principals WHERE name = 'usr_consulta')    DROP LOGIN usr_consulta;
+IF EXISTS (SELECT 1 FROM sys.server_principals WHERE name = 'usr_importador')  DROP LOGIN usr_importador;
+GO
+ 
+IF EXISTS (SELECT 1 FROM sys.database_principals WHERE name = 'rol_admin'      AND type = 'R') DROP ROLE rol_admin;
+IF EXISTS (SELECT 1 FROM sys.database_principals WHERE name = 'rol_ventas'     AND type = 'R') DROP ROLE rol_ventas;
+IF EXISTS (SELECT 1 FROM sys.database_principals WHERE name = 'rol_consulta'   AND type = 'R') DROP ROLE rol_consulta;
+IF EXISTS (SELECT 1 FROM sys.database_principals WHERE name = 'rol_importador' AND type = 'R') DROP ROLE rol_importador;
+GO
+ 
 
 -- CREACION DE ROLES
 
@@ -67,19 +95,29 @@ GO
 -- Ejecuta SPs de logica de negocio y ABM de visitantes
 -- y puntos de venta. No accede directamente a tablas.
 
-
+GRANT SELECT ON maestros.TipoVisitante TO rol_ventas;
+GRANT EXECUTE ON TYPE::dbo.TipoEntradaDetalle TO rol_ventas;
+GRANT EXECUTE ON OBJECT::dbo.sp_VentaEntradaSimple TO rol_ventas;
+GRANT SELECT ON maestros.TipoVisitante TO rol_ventas;
 -- SPs de logica de negocio (estan en dbo)
 GRANT EXECUTE ON OBJECT::dbo.sp_VentaEntradas         TO rol_ventas;
 GRANT EXECUTE ON OBJECT::dbo.sp_ContratarActividad    TO rol_ventas;
 GRANT EXECUTE ON OBJECT::dbo.sp_AsignarGuiaATour      TO rol_ventas;
 GRANT EXECUTE ON OBJECT::dbo.sp_AnularTicket          TO rol_ventas;
-
+ 
 -- SPs ABM necesarios para operar
 GRANT EXECUTE ON OBJECT::dbo.sp_Visitante_Insertar    TO rol_ventas;
 GRANT EXECUTE ON OBJECT::dbo.sp_Visitante_Actualizar  TO rol_ventas;
 GRANT EXECUTE ON OBJECT::dbo.sp_PuntoVenta_Insertar   TO rol_ventas;
 GRANT EXECUTE ON OBJECT::dbo.sp_PuntoVenta_Actualizar TO rol_ventas;
-
+ 
+-- SELECT solo sobre las tablas necesarias para operar la pantalla de ventas
+GRANT SELECT ON ventas.Visitante     TO rol_ventas;
+GRANT SELECT ON parques.Parque       TO rol_ventas;
+GRANT SELECT ON parques.PuntoVenta   TO rol_ventas;
+GRANT SELECT ON maestros.FormaPago   TO rol_ventas;
+GRANT SELECT ON ventas.Ticket        TO rol_ventas;
+ 
 -- Denegar DML directo (DENY es correcto aqui: nunca debe
 -- hacerse INSERT/UPDATE/DELETE directo sobre las tablas)
 DENY INSERT ON SCHEMA::ventas      TO rol_ventas;
@@ -87,22 +125,24 @@ DENY INSERT ON SCHEMA::maestros    TO rol_ventas;
 DENY INSERT ON SCHEMA::parques     TO rol_ventas;
 DENY INSERT ON SCHEMA::atracciones TO rol_ventas;
 DENY INSERT ON SCHEMA::concesiones TO rol_ventas;
-
+ 
 DENY UPDATE ON SCHEMA::ventas      TO rol_ventas;
 DENY UPDATE ON SCHEMA::maestros    TO rol_ventas;
 DENY UPDATE ON SCHEMA::parques     TO rol_ventas;
 DENY UPDATE ON SCHEMA::atracciones TO rol_ventas;
 DENY UPDATE ON SCHEMA::concesiones TO rol_ventas;
-
+ 
 DENY DELETE ON SCHEMA::ventas      TO rol_ventas;
 DENY DELETE ON SCHEMA::maestros    TO rol_ventas;
 DENY DELETE ON SCHEMA::parques     TO rol_ventas;
 DENY DELETE ON SCHEMA::atracciones TO rol_ventas;
 DENY DELETE ON SCHEMA::concesiones TO rol_ventas;
-
+ 
 -- SELECT directo: no se otorga (REVOKE implicito).
+-- No se hace DENY SELECT para no romper los SPs en dbo
+-- que internamente leen tablas de otros schemas.
 GO
-
+ 
 
 -- ROL: rol_consulta
 -- Solo ejecuta SPs de reportes. No modifica datos
@@ -247,4 +287,7 @@ ORDER BY s.name, o.name;
 -- Todos los SPs deben aparecer bajo maestros, parques, ventas,
 -- atracciones, concesiones, importacion o negocio.
 GO
+
+
+
 
