ebad222a785216732640523cc609c5227916c5cb
Santino Rolleri Villalba <167250755+Srv129@users.noreply.github.com>
2026-07-02 19:41:16 -0300

    cambios
    Restored header comments and procedures for reports in sps reportes.sql.

--- db/sps reportes.sql
+++ db/sps reportes.sql
@@ -3,7 +3,7 @@
   Universidad: Universidad Nacional de la Matanza
   Materia:     3641 - Bases de Datos Aplicada
   Grupo:       9
-  Integrantes: Caro, Melisa; Rolleri Villalba, Santino; Llanos, Franco; Vazquez, Nahuel Dominicano
+  Integrantes: Caro, Melisa; Rolleri Villalba, Santino; Llanos, Franco; Vazquez.
   Fecha:       12/06/2026
   Descripcion: Stored Procedures de reportes.
                Los reportes 2 y 5 retornan XML segun
